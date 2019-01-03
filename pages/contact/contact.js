b1cf77b5de7aeff065eadfc71af8f3bcc251b0c0
yxman.Huang <498571187@qq.com>
2019-01-03 15:41:22 +0800

    Initial Commit

---
+++ pages/contact/contact.js
@@ -0,0 +1,62 @@
+// pages/contact/contact.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    markers: [{
+      iconPath: "/img/hot_se.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 25,
+      height: 25
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color: "#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '/img/hot.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    regionchange(e) {
+      console.log(e.type)
+    },
+    markertap(e) {
+      console.log(e.markerId)
+    },
+    controltap(e) {
+      console.log(e.controlId)
+    }
+
+  }
+})
